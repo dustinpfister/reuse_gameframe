07793afa3eb8b895d8ac87bc1a77f3c0834aa6cc
dustinpfister <dustin.pfister@gmail.com>
2025-10-14 16:31:06 -0400

    have a starting form of a 'ROM'

--- changelog.md
+++ changelog.md
@@ -2,13 +2,13 @@
 
 RMC = 2769314.05 / 365 = 7587.16;
 IRC = 1998875.80 / 365 = 5476.37;
-
 ( 7587.16 + 5476.37 ) / 2 = 6531.76;
 
 ## pending for current
-* have a starting form of a 'ROM'
+
 
 ## R0 () - Core Idea of Project working
+* have a starting form of a 'ROM'
 * automate the process of getting up to date stats from data1
 * get\_production\_data.js module started that can be used to get current production data
 * added Phaser 3.90 as part of the over all source code of the framework
--- src/main_index.js
+++ src/main_index.js
@@ -8,6 +8,70 @@ const canvas = document.createElement('canvas');
 canvas.setAttribute('style', 'display: block;margin-left: auto;margin-right: auto;');
 canvas.width = 640;
 canvas.height = 480;
+/********* **********
+ROM PROTOTYPE
+********** *********/
+const push_text = (disp, result, store='RMC') => {
+    disp.text += 'store: ' + store + '\n';
+    Object.keys( result[store].tb ).forEach((key) => {
+        const n = result[store].tb[key];
+        disp.text += key + ': ' + n + '\n' ;
+    });
+    disp.text += '\n\n';
+};
+const get_price_per = ( result, store='RMC', avg=6531.76) => {
+    const pricing = result[store].tb['Total Pricing ($)'];
+    let per = pricing / ( avg * 2 );
+    per = per < 0 ? 0 : per;
+    per = per > 1 ? 1 : per;
+    return parseFloat( per.toFixed(2) );
+};
+const Rom = {
+
+    create: ( Rom, scene ) => {
+        const disp = scene.add.text(10, 10, '', {  });
+        scene.game.registry.set('disp', disp);
+        const texture = scene.textures.createCanvas('meter', 256, 256);
+        scene.registry.set('texture', texture);
+        // add frames
+        // name, sourceIndex, x, y, w, h
+        texture.add(0, 0, 0,   0, 256, 128);
+        texture.add(1, 0, 0, 128, 256, 128);
+        scene.add.sprite(128 + 320, 64 + 20, 'meter', 0);
+        scene.add.sprite(128 + 320, 64 + 20 + 128 + 20, 'meter', 1);
+        Rom.draw(Rom, scene);
+    },
+    
+    draw: (Rom, scene) => {
+        const texture = scene.registry.get('texture');
+        const ctx = texture.context;
+        let r = Math.floor( Math.random() * 3 );
+        ctx.fillStyle = 'red,blue,green'.split(',').slice(r, r + 1);
+        ctx.fillRect(0,   0, 256, 128);
+        r = Math.floor( Math.random() * 3 );
+        ctx.fillStyle = 'yellow,white,orange'.split(',').slice(r, r + 1);
+        ctx.fillRect(0, 128, 256, 128);
+        texture.refresh();
+    },
+    
+    pull: (Rom, scene, result, date_now) => {
+    
+        const disp = scene.game.registry.get('disp');    
+        disp.text = '';
+        push_text(disp, result, 'RMC');
+        push_text(disp, result, 'IRC');
+                    
+        console.log('new pull for: ' + date_now);
+        console.log('RMC pricing: ' + result.RMC.tb['Total Pricing ($)']);
+        console.log('IRC pricing: ' + result.IRC.tb['Total Pricing ($)']);
+        console.log('RMC per: ' + get_price_per(result, 'RMC') + ', IRC per: ' + get_price_per(result, 'IRC') );
+        console.log('');
+        Rom.draw(Rom, scene);
+    }
+
+}
+
+
 class Boot extends Phaser.Scene {
     constructor (config) {
         super(config);
@@ -15,24 +79,12 @@ class Boot extends Phaser.Scene {
     }
     create () {
         const scene = this;
-        const disp = this.add.text(10, 10, '', {  });
+        const game = scene.game;
         
-        const push_text = (disp, result, store='RMC') => {
-            disp.text += 'store: ' + store + '\n';
-            Object.keys( result[store].tb ).forEach((key) => {
-                const n = result[store].tb[key];
-                disp.text += key + ': ' + n + '\n' ;
-            });
-            disp.text += '\n\n';
-        };
+        Rom.create(Rom, scene);
         
-        const get_price_per = ( result, store='RMC', avg=6531.76) => {
-            const pricing = result[store].tb['Total Pricing ($)'];
-            let per = pricing / ( avg * 2 );
-            per = per < 0 ? 0 : per;
-            per = per > 1 ? 1 : per;
-            return parseFloat( per.toFixed(2) );
-        };
+  
+
         
         
         game.registry.set('PULL_LT', new Date(0) );
@@ -48,15 +100,8 @@ class Boot extends Phaser.Scene {
                 game.registry.set('PULL_LT', date_now);        
                 get()
                 .then((result)=>{
-                    disp.text = '';
-                    push_text(disp, result, 'RMC');
-                    push_text(disp, result, 'IRC');
                     
-                    console.log('new pull for: ' + date_now);
-                    console.log('RMC pricing: ' + result.RMC.tb['Total Pricing ($)']);
-                    console.log('IRC pricing: ' + result.IRC.tb['Total Pricing ($)']);
-                    console.log('RMC per: ' + get_price_per(result, 'RMC') + ', IRC per: ' + get_price_per(result, 'IRC') );
-                    console.log('');
+                    Rom.pull(Rom, scene, result, date_now);
                     
                 });
                 
